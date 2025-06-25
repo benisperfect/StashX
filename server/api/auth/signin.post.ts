58a073b2655255d7d0299f73f319d535f1774f04
HOANG SY LAM <perfectionofcreation@gmail.com>
2025-06-25 15:55:14 +0700

    sd

--- server/api/auth/signin.post.ts
+++ server/api/auth/signin.post.ts
@@ -1,14 +1,14 @@
 import { defineEventHandler, readBody, setCookie } from 'h3';
 import { writeFile, mkdir, readFile } from 'fs/promises';
 
-const userPath = './server/data/users.json';
+const userPath = '/data/users.json';
 
 export default defineEventHandler(async (event) => {
   const body = await readBody(event);
   const { email, password, firstName, lastName, username } = body;
 
   try {
-    await mkdir('./server/data', { recursive: true });
+    await mkdir('/data', { recursive: true });
     let users: { email: string; password: string, firstName: string, lastName: string, username: string }[] = [];
 
     try {
